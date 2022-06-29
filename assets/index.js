aab0ae83656db224162757da42a4497997acf7b2
carreuky <carreuky@gmail.com>
2022-06-29 22:05:59 +0300

    additions

--- assets/index.js
+++ assets/index.js
@@ -4,9 +4,11 @@ const wrapper = document.querySelector(".wrapper"),
   volume = wrapper.querySelector(".word i"),
   removeIcon = wrapper.querySelector(".search span"),
   infoText = wrapper.querySelector(".info-text");
+  clearIcon=wrapper.querySelector('.search span')
+
 let audio;
 
-// fetch api function
+
 function fetchApi(word) {
   infoText.style.color = "#000";
   wrapper.classList.remove("active");
@@ -14,5 +16,54 @@ function fetchApi(word) {
   let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
   fetch(url)
     .then((res) => res.json())
-    .then((result) => data(result, word));
+    .then((result) => data(result, searchInput.value))
+    .catch(err=>console.log(err))
 }
+console.log(searchInput.value)
+
+const data= (result , input)=>{
+ if(result.title){
+   infoText.innerHTML=`Error! ,${word} cannot be found`
+ }
+ else{
+   wrapper.classList.add('active')
+   let wordFromAPI=result[0].word
+   let def=result[0].meanings[0].definitions[0]
+  //  console.log(result[0].phonetics[0].text)
+   phonetics=`Commonly pronounced as : ${result[0].phonetics[0].text}`
+   document.querySelector('.word span').innerText=phonetics
+   document.querySelector('.word p').innerText=wordFromAPI.charAt(0).toUpperCase() + wordFromAPI.slice(1)
+   document.querySelector('.meaning span').innerText=def.definition
+   document.querySelector('.example span').innerText=def.example ?? 'No example to illustrate'
+   document.querySelector('.synonym .list').innerText=result[0].meanings[0].synonyms.map(ele=>ele)
+  //  console.log(result[0])
+
+ }
+  // console.log(result)
+}
+clearIcon.addEventListener('click',()=>{
+  searchInput.value=''
+  searchInput.focus()
+  wrapper.classList.remove('active')
+  infoText.style.color = "#9a9a9a";
+  infoText.innerHTML=`Type any existing word and press enter to get meaning, example,
+  synonyms, etc.`
+
+})
+
+
+// function fetchApi(word) {
+//   infoText.style.color = "#000";
+//   wrapper.classList.remove("active");
+//   infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
+//   infoText.innerHTML=`${word}`
+//   let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((result) => console.log(result) );
+// }
+searchInput.addEventListener('keydown' , (e)=>{
+  if (e.key === 'Enter') {
+    fetchApi(searchInput.value)
+  }
+})
